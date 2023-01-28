61f7b30c769ea82d13ed01015e8a60260576778f
rayhan <prog.rayhan1@gmail.com>
2023-01-28 14:33:50 +0600

    django templates added

--- DjangoW3/my_tennis_club/members/views.py
+++ DjangoW3/my_tennis_club/members/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+# def member(request):
+#     return HttpResponse("Hello World!")
+
 def member(request):
-    return HttpResponse("Hello World!")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
